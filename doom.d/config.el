97df6a735be7a54bc6c18468dedd44fd0e55dd6b
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2020-04-12 14:18:38 +0300

    Add org-roam locations

--- doom.d/config.el
+++ doom.d/config.el
@@ -31,6 +31,7 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/"
+      org-roam-directory "~/org/roam/"
       org-bullets-bullet-list '("☰" "☱" "☲" "☳" "☴" "☵" "☶" "☷" "☷" "☷" "☷"))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
